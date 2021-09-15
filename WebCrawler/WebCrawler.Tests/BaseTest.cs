d0e91cc10dc01be1c4a5c2114a6b214427a8d3f1
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 18:22:16 +0430

    add WebCrawler.Tests Project

---
+++ WebCrawler/WebCrawler.Tests/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace WebCrawler.Tests
+{
+    public class BaseTest
+    {
+        public ServiceProvider serviceProvider { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            services.AddServices();
+            serviceProvider = services.BuildServiceProvider();
+        }
+    }
+}
