31a9d037428dd483d207019c015ea3469ff8640a
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 18:38:47 +0430

    add HttpHandlerTests

---
+++ WebCrawler/WebCrawler.Tests/HttpHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler.Tests
+{
+    public class HttpHandlerTests:BaseTest
+    {
+        [Test]
+        public async Task testSuccessToGetStringAsyncIfGivenUrlIsValid()
+        {
+            string url = "https://tretton37.com";
+            var _httpHandler = (IHttpHandler)serviceProvider.GetRequiredService(typeof(IHttpHandler));
+
+            var result = await _httpHandler.GetStringAsync(url);
+
+            Assert.AreEqual(result != null, true);
+        }
+
+        [Test]
+        public async Task testFailedToGetStringAsyncIfGivenUrlIsInvalid()
+        {
+            string url = "https://tretton37";
+            var _httpHandler = (IHttpHandler)serviceProvider.GetRequiredService(typeof(IHttpHandler));
+
+            var result = await _httpHandler.GetStringAsync(url);
+
+            Assert.AreEqual(result == null, true);
+        }
+    }
+}
