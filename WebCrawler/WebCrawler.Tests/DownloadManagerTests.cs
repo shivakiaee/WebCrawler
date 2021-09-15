692a5ff8f76edb754c6819bd023889a81dd320eb
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 19:36:29 +0430

    add DownloadManagerTests

---
+++ WebCrawler/WebCrawler.Tests/DownloadManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler.Tests
+{
+    public class DownloadManagerTests:BaseTest
+    {
+        [Test]
+        public async Task testSuccessToDownloadPageIfGivenUrlIsValid()
+        {
+            var _downloadManager = (IDownloadManager)serviceProvider.GetRequiredService(typeof(IDownloadManager));
+            var result = await _downloadManager.DownloadPage("https://tretton37.com/who-we-are");
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public async Task testFailedToDownloadPageIfGivenUrlIsInvalid()
+        {
+            var _downloadManager = (IDownloadManager)serviceProvider.GetRequiredService(typeof(IDownloadManager));
+            var result = await _downloadManager.DownloadPage("www.google.com");
+            Assert.AreEqual(result, false);
+        }
+    }
+}
