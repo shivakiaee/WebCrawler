0bf86d2418b05080abe5d9abecd22a7ae81ee649
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 19:04:26 +0430

    add CrawlerTests

---
+++ WebCrawler/WebCrawler.Tests/CrawlerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebCrawler.Models;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler.Tests
+{
+    public class CrawlerTests:BaseTest
+    {
+        [Test]
+        public async Task testSuccessToCrawlIfUrlIsValid()
+        {
+            var _crawler = (ICrawler)serviceProvider.GetRequiredService(typeof(ICrawler));
+
+            var linkQueues = new List<LinkQueue>();
+            linkQueues.Add(new LinkQueue()
+            {
+                Link = "https://tretton37.com",
+                IsCrawled = true,
+            });
+
+            //better approch is using of Mock http server
+            await _crawler.Crwal("https://tretton37.com/who-we-are", linkQueues);
+
+            Assert.AreEqual(linkQueues[1].Link, "https://tretton37.com/who-we-are");
+        }
+
+        [Test]
+        public async Task testFailedToCrawlIfUrlIsInvalid()
+        {
+            var _crawler = (ICrawler)serviceProvider.GetRequiredService(typeof(ICrawler));
+
+            var linkQueues = new List<LinkQueue>();
+            linkQueues.Add(new LinkQueue()
+            {
+                Link = "https://tretton37.com",
+                IsCrawled = true,
+            });
+
+            //better approch is using of Mock http server
+            string invalidUrl = "https://tretton37";
+            await _crawler.Crwal(invalidUrl, linkQueues);
+
+            Assert.AreEqual(linkQueues.Count, 1);
+        }
+    }
+}
