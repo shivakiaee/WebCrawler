a1bbb08c3a91bde159437687747cec330c32add0
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 18:19:04 +0430

    add HtmlDocumentHandler

---
+++ WebCrawler/WebCrawler/Services/HtmlDocumentHandler.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler.Services
+{
+    /// <summary>
+    /// Is responsible for working with html documents
+    /// </summary>
+    public class HtmlDocumentHandler: IHtmlDocumentHandler
+    {
+
+        /// <summary>
+        /// Is responsible for loading a html doc and go through it to find specified tag
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public IEnumerable<HtmlNode> GetDescendantsNode(string html,string tag)
+        {
+            try
+            {
+                var HtmllDoc = new HtmlDocument();
+                HtmllDoc.LoadHtml(html);
+                var node = HtmllDoc.DocumentNode;
+                return node.Descendants(tag);
+            }
+            catch
+            {
+                return null;
+            }
+   
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler/Services/Interfaces/IHtmlDocumentHandler.cs
@@ -0,0 +1,10 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace WebCrawler.Services.Interfaces
+{
+    public interface IHtmlDocumentHandler
+    {
+        IEnumerable<HtmlNode> GetDescendantsNode(string html, string tag);
+    }
+}
