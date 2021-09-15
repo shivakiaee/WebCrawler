a96c8b52cd155150def5a0da10be2ae9ea2df4ea
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 19:46:44 +0430

    add IWorker

---
+++ WebCrawler/WebCrawler/Services/Interfaces/IWorker.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebCrawler.Models;
+
+namespace WebCrawler.Services.Interfaces
+{
+    public interface IWorker
+    {
+        public string StartUrl { get; set; }
+        public List<LinkQueue> LinksQueue { get; }
+        void Do();
+    }
+}
---
+++ WebCrawler/WebCrawler/Services/Worker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCrawler.Models;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler.Services
+{
+    /// <summary>
+    /// Is responsible for reading from a queue and creates and runs some tasks (limited)
+    /// </summary>
+    public class Worker : IWorker
+    {
+        private List<LinkQueue> _linkQueues = new List<LinkQueue>();
+        public List<LinkQueue> LinksQueue => _linkQueues;
+        private List<Task> TaskList;
+        private readonly IDownloader _downloader;
+
+        public string StartUrl { get; set; }
+
+        public Worker(IDownloader downloader)
+        {
+            _downloader = downloader;
+        }
+
+        /// <summary>
+        /// Starts reading links from queue and creates and runs tasks
+        /// </summary>
+        public void Do()
+        {
+            _linkQueues.Add(new LinkQueue()
+            {
+                Link = StartUrl,
+                IsCrawled = false,
+            });
+
+            while (true)
+            {
+
+                //It is better to get number of parallel tasks(5 hardcoded) from input
+                var linksToCrawl = _linkQueues.Where(l => !l.IsCrawled).Take(5).ToList();
+                TaskList = new List<Task>();
+                for (int i = 0; i < _linkQueues.Where(l => !l.IsCrawled).Count(); i++)
+                {
+                    TaskList.Add(_downloader.Download(linksToCrawl[i].Link, _linkQueues));
+                }
+                Task.WaitAll(TaskList.ToArray());
+
+                if (_linkQueues.Count == 0)
+                {
+                    //todo: finish the app and show a goodby message
+                }
+            }
+
+        }
+    }
+}
