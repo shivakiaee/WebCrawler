8e84abc33c3a072b16bded5457cac78c338f4cb9
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 18:28:21 +0430

    add HtmlDocumentHandlerTests

---
+++ WebCrawler/WebCrawler.Tests/HtmlDocumentHandlerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Linq;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler.Tests
+{
+    public class HtmlDocumentHandlerTests:BaseTest
+    {
+        [Test]
+        public void testSuccessToGetDescendantsNodeIfGivenHtmlIsValid()
+        {
+            var _htmlDocumentHandler = (IHtmlDocumentHandler)serviceProvider.GetRequiredService(typeof(IHtmlDocumentHandler));
+            var html = "<!DOCTYPE html><a href=\"https://www.w3schools.com\">Visit W3Schools.com!</a></html>";
+            var nodes = _htmlDocumentHandler.GetDescendantsNode(html, "a").ToList();
+
+            Assert.AreEqual(nodes.Count, 1);
+        }
+
+        [Test]
+        public void testFailedToGetDescendantsNodeIfGivenHtmlIsNull()
+        {
+            var _htmlDocumentHandler = (IHtmlDocumentHandler)serviceProvider.GetRequiredService(typeof(IHtmlDocumentHandler));
+            var nodes = _htmlDocumentHandler.GetDescendantsNode(null, "a");
+
+            Assert.AreEqual(nodes, null);
+        }
+    }
+}
