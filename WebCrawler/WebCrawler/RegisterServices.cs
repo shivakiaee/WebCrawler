f3c14b64a771d52c8dde1f50c62150579c817047
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 18:19:57 +0430

    add RegisterServices

---
+++ WebCrawler/WebCrawler/RegisterServices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebCrawler.Services;
+using WebCrawler.Services.Interfaces;
+
+namespace WebCrawler
+{
+    /// <summary>
+    /// Is responsible for registering services to support DI
+    /// </summary>
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IHtmlDocumentHandler, HtmlDocumentHandler>();
+            return services;
+        }
+    }
+}
