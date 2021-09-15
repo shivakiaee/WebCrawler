3b3825e808b323b8d33072165f3d1dc4fb66d395
shiva.kiaee@gmail.com <shiva.kiaee@gmail.com>
2021-09-15 18:41:25 +0430

    add LinkQueueExtension to extend LinkQueue functionality
    add Find
    add FirstItem

---
+++ WebCrawler/WebCrawler/Extensions/LinkQueueExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebCrawler.Models;
+
+namespace WebCrawler.Extensions
+{
+    public static class LinkQueueExtension
+    {
+        public static LinkQueue Find(this List<LinkQueue> linkQueues,string link)
+        {
+            if (string.IsNullOrEmpty(link))
+                return null;
+
+            if (linkQueues.Count == 0)
+                return null;
+
+            return linkQueues.Where(lq => lq.Link == link).FirstOrDefault();
+        }
+
+        public static LinkQueue FirstItem(this List<LinkQueue> linkQueues)
+        {
+            if (linkQueues.Count == 0)
+                return null;
+
+            return linkQueues.FirstOrDefault();
+        }
+    }
+}
